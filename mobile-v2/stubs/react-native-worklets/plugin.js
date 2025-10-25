37541b65b35c1599ff9260549ead4fcb49c7cfe3
Lucas Amaral <lucas.trevvos@gmail.com>
2025-10-25 00:02:49 -0300

    chore: salvar estado antes do prebuild

--- mobile-v2/stubs/react-native-worklets/plugin.js
+++
@@ -1,5 +0,0 @@
-// stubs/react-native-worklets/plugin.js
-module.exports = function fakeWorkletsPlugin() {
-  // Plugin vazio só pra satisfazer o require do babel-preset-expo
-  return { visitor: {} };
-};
