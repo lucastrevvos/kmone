274228c0cb06189fcb24d0e0c6636e6db3be41f1
Lucas Amaral <lucas.trevvos@gmail.com>
2025-11-06 16:08:02 -0300

    fix: remove org.gradle.java.home for EAS Linux

--- mobile-v2/babel.config.js
+++ mobile-v2/babel.config.js
@@ -5,5 +5,6 @@ module.exports = function (api) {
       ["babel-preset-expo", { jsxImportSource: "nativewind" }],
       "nativewind/babel",
     ],
+    plugins: [require("react-native-worklets/plugin")], // 👈 adiciona isto
   };
 };
